35d0d955473f5a72168b1b0a6284a39202360f9c
Jinwook Kim <redwoodnatlpark@gmail.com>
2021-06-20 08:31:03 +0900

    Add features

--- Codewell00/Controllers/ViewController.swift
+++ Codewell00/Controllers/ViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var subtitleLabel: UILabel!
     @IBOutlet var discountLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,11 +19,18 @@ class ViewController: UIViewController {
 extension ViewController {
     func initialize() {
         self.initializeDiscountLabel()
+        self.initializeSubtitleLabel()
     }
     func initializeDiscountLabel() {
         let string: String = "You will be receiving 100% of the earnings. 🎉"
         let underlineAttribute: [NSAttributedString.Key: Int] = [NSAttributedString.Key.underlineStyle: NSUnderlineStyle.single.rawValue]
         let underlineAttributedString: NSAttributedString = NSAttributedString(string: string, attributes: underlineAttribute)
         self.discountLabel.attributedText = underlineAttributedString
+    }
+    func initializeSubtitleLabel() {
+        let string: String = "Spense is an open platform for individuals to share their unfiltered thoughts. Discuss the topics you love, and get paid for doing just that."
+        let italicFont: UIFont = UIFont(name: "Helvetica", size: 18)!
+        let attributedString: NSMutableAttributedString = NSMutableAttributedString(string: string, attributes: [NSAttributedString.Key.font: italicFont])
+        attributedString.addAttribute(NSAttributedString.Key.font, value: italicFont, range: NSMakeRange(30, 36))
     }
 }
