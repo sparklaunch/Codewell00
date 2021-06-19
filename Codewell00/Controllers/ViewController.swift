03f7eef155eaf7e34b7ff49142940c48e613fce4
Jinwook Kim <redwoodnatlpark@gmail.com>
2021-06-19 16:24:38 +0900

    Finish discount header

--- Codewell00/Controllers/ViewController.swift
+++ Codewell00/Controllers/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var discountLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.initialize()
     }
-
-
 }
 
+extension ViewController {
+    func initialize() {
+        self.initializeDiscountLabel()
+    }
+    func initializeDiscountLabel() {
+        let string: String = "You will be receiving 100% of the earnings. 🎉"
+        let underlineAttribute: [NSAttributedString.Key: Int] = [NSAttributedString.Key.underlineStyle: NSUnderlineStyle.single.rawValue]
+        let underlineAttributedString: NSAttributedString = NSAttributedString(string: string, attributes: underlineAttribute)
+        self.discountLabel.attributedText = underlineAttributedString
+    }
+}
