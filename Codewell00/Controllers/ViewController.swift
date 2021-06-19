5ce6a0f0145f500109917ca322000cadb3638c68
Jinwook Kim <redwoodnatlpark@gmail.com>
2021-06-20 08:51:21 +0900

    Add email textfield

--- Codewell00/Controllers/ViewController.swift
+++ Codewell00/Controllers/ViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var getStartedTextField: UITextField!
+    @IBOutlet var getStartedButton: UIButton!
     @IBOutlet var subtitleLabel: UILabel!
     @IBOutlet var discountLabel: UILabel!
     override func viewDidLoad() {
@@ -20,6 +22,8 @@ extension ViewController {
     func initialize() {
         self.initializeDiscountLabel()
         self.initializeSubtitleLabel()
+        self.initializeGetStartedButton()
+        self.initializeGetStartedTextField()
     }
     func initializeDiscountLabel() {
         let string: String = "You will be receiving 100% of the earnings. 🎉"
@@ -32,5 +36,15 @@ extension ViewController {
         let italicFont: UIFont = UIFont(name: "Helvetica", size: 18)!
         let attributedString: NSMutableAttributedString = NSMutableAttributedString(string: string, attributes: [NSAttributedString.Key.font: italicFont])
         attributedString.addAttribute(NSAttributedString.Key.font, value: italicFont, range: NSMakeRange(30, 36))
+    }
+    func initializeGetStartedButton() {
+        self.getStartedButton.layer.cornerRadius = 5.0
+    }
+    func initializeGetStartedTextField() {
+        self.getStartedTextField.layer.cornerRadius = 5.0
+        self.getStartedTextField.layer.borderWidth = 1
+        self.getStartedTextField.layer.borderColor = UIColor.systemGray.cgColor
+        self.getStartedTextField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 15, height: self.getStartedTextField.frame.size.height))
+        self.getStartedTextField.leftViewMode = .always
     }
 }
